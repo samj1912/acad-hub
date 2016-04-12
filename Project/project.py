334426325c88b6ebf993256e5ee65094bd241657
Maulik <patnimaulik96@gmail.com>
2016-04-13 02:49:41 +0530

    Started notes uploader

--- Project/project.py
+++ Project/project.py
@@ -95,6 +95,71 @@ class MainNotebook(Gtk.Window):
         self.notebook.append_page(self.page3, Gtk.Label('Course Information'))
         #adding the course page to notebook view
 
+
+        self.page4 = Gtk.Box()
+        self.page4.set_border_width(10)
+        
+        button1 = Gtk.Button("Choose File")
+        button1.connect("clicked", self.on_file_clicked)
+        self.page4.add(button1)
+
+        button2 = Gtk.Button("Choose Folder")
+        button2.connect("clicked", self.on_folder_clicked)
+        self.page4.add(button2)
+
+        self.notebook.append_page(self.page4, Gtk.Label('Notes'))
+
+
+
+    def on_file_clicked(self, widget):
+        dialog = Gtk.FileChooserDialog("Please choose a file", self,
+            Gtk.FileChooserAction.OPEN,
+            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+             Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+
+        self.add_filters(dialog)
+
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            print("Open clicked")
+            print("File selected: " + dialog.get_filename())
+        elif response == Gtk.ResponseType.CANCEL:
+            print("Cancel clicked")
+
+        dialog.destroy()
+
+    def add_filters(self, dialog):
+        filter_text = Gtk.FileFilter()
+        filter_text.set_name("Text files")
+        filter_text.add_mime_type("text/plain")
+        dialog.add_filter(filter_text)
+
+        filter_py = Gtk.FileFilter()
+        filter_py.set_name("Python files")
+        filter_py.add_mime_type("text/x-python")
+        dialog.add_filter(filter_py)
+
+        filter_any = Gtk.FileFilter()
+        filter_any.set_name("Any files")
+        filter_any.add_pattern("*")
+        dialog.add_filter(filter_any)
+
+    def on_folder_clicked(self, widget):
+        dialog = Gtk.FileChooserDialog("Please choose a folder", self,
+            Gtk.FileChooserAction.SELECT_FOLDER,
+            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+             "Select", Gtk.ResponseType.OK))
+        dialog.set_default_size(800, 400)
+
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            print("Select clicked")
+            print("Folder selected: " + dialog.get_filename())
+        elif response == Gtk.ResponseType.CANCEL:
+            print("Cancel clicked")
+
+        dialog.destroy()
+
 class MainBox(Gtk.Window):
 
     def __init__(self):
