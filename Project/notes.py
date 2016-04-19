26f190fa12061473b2957a19367c348b195aa273
Maulik <patnimaulik96@gmail.com>
2016-04-19 19:03:14 +0530

    Changed server ip

--- Project/notes.py
+++ Project/notes.py
@@ -44,7 +44,7 @@ def downloadFile(filename, filepath, course, roll):
 	url = 'http://172.16.115.76/download.php'
 	data = {'course':course, 'file':filename, 'roll':roll}
 	r = requests.post(url, data=data)
-	print r.text
+	# print r.text
 	with open(filepath+'/'+filename, 'wb') as f:
 		f.write(r.content)
 	print "Yayy!"
